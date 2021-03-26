ba39d027f3efaae5fc3580da32c293d11cd3e752
王宇 <coder_wangyu@163.com>
2021-03-26 14:37:45 +0800

    1.集成cesium

---
+++ src/views/entry.vue
@@ -0,0 +1,15 @@
+<template>
+	<div id="entry">
+
+	</div>
+</template>
+
+<script>
+export default {
+	name: "entry",
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	lintOnSave: false,
+	devServer: {
+		open: true,
+		port: 10090,
+		overlay: {
+			errors: false,
+			warnings: true
+		}
+	},
+}
